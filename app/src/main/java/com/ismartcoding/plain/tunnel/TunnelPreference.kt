7c89948a71a4fabbcfcb121cf08bdbb6c4f508e5
Meon Sarkar <meon@kali>
2026-07-15 03:45:55 +0600

    fix: missing stringPreferencesKey import

--- app/src/main/java/com/ismartcoding/plain/tunnel/TunnelPreference.kt
+++ app/src/main/java/com/ismartcoding/plain/tunnel/TunnelPreference.kt
@@ -2,6 +2,7 @@ package com.ismartcoding.plain.tunnel
 
 import com.ismartcoding.plain.preferences.BasePreference
 import androidx.datastore.preferences.core.booleanPreferencesKey
+import androidx.datastore.preferences.core.stringPreferencesKey
 
 object TunnelEnabledPreference : BasePreference<Boolean>() {
     override val default = true
